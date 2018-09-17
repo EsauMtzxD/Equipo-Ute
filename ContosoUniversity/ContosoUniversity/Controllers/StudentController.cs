fa1f009eef6096d4dd81e1da53ddadb3ac7d2e41
EsauMtzxD <43343106+EsauMtzxD@users.noreply.github.com>
2018-09-17 12:46:36 -0500

    Cree el Controlador de la clase student como las operaciones CRUD de la misma

---
+++ ContosoUniversity/ContosoUniversity/Controllers/StudentController.cs
@@ -0,0 +1,234 @@
+﻿using ContosoUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContosoUniversity.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        private ContosoUniversityContext dbCtx = new ContosoUniversityContext();
+
+        #region Index and Details
+        // GET: Student
+        public ActionResult Index()
+        {
+
+            List<Student> students = new List<Student>();
+
+            students = dbCtx.Students.OrderBy(x => x.LastName).ToList();
+
+
+
+            return View(students);
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+
+            Student student = dbCtx.Students.Find(id);
+
+            if(student == null)
+            {
+
+                return HttpNotFound();
+
+            }
+
+            return View(student);
+        }
+        #endregion
+
+        #region Create camp
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+
+                        dbCtx.Students.Add(student);
+                        dbCtx.SaveChanges();
+
+
+                    }//End Second Try
+                    catch (DbEntityValidationException e)
+                    {
+
+                        foreach (var eve in e.EntityValidationErrors)
+                        {
+
+                            Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors: ",
+                                eve.Entry.Entity.GetType().Name, eve.Entry.State);
+
+                            foreach (var ve in eve.ValidationErrors)
+                            {
+
+                                Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                                    ve.PropertyName, ve.ErrorMessage);
+
+                            }//End Second foreach
+
+                        }//End first foreach
+                        throw;
+
+                    }//End Catch
+
+                }//End the IF
+
+                return RedirectToAction("Index");
+            }//End Secod Try
+            catch
+            {
+                return View();
+            }//End Catch
+        }
+        #endregion
+
+        #region Edit the camp
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+
+            Student student = dbCtx.Students.Find(id);
+
+            if(student == null)
+            {
+
+                return HttpNotFound();
+
+            }
+
+            return View(student);
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Student student)
+        {
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+
+                    try
+                    {
+
+                        dbCtx.Entry(student).State = System.Data.Entity.EntityState.Modified;
+                        dbCtx.SaveChanges();
+
+                    }
+                    catch (DbEntityValidationException e)
+                    {
+
+                        foreach (var eve in e.EntityValidationErrors)
+                        {
+
+                            Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors: ",
+                                eve.Entry.Entity.GetType().Name, eve.Entry.State);
+
+                            foreach (var ve in eve.ValidationErrors)
+                            {
+
+                                Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                                    ve.PropertyName, ve.ErrorMessage);
+
+                            }
+
+                        }
+                        throw;
+
+                    }
+
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        #endregion
+
+        #region Delete the camp
+        // GET: Student/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            Student student = dbCtx.Students.Find(id);
+
+            if(student == null)
+            {
+
+                return HttpNotFound();
+
+            }
+
+            return View(student);
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+
+                try
+                {
+
+                    Student student = dbCtx.Students.Find(id);
+
+                    dbCtx.Students.Remove(student);
+                    dbCtx.SaveChanges();
+
+                }
+                catch (DbEntityValidationException e)
+                {
+
+                    foreach (var eve in e.EntityValidationErrors)
+                    {
+
+                        Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors: ",
+                            eve.Entry.Entity.GetType().Name, eve.Entry.State);
+
+                        foreach (var ve in eve.ValidationErrors)
+                        {
+
+                            Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                                ve.PropertyName, ve.ErrorMessage);
+
+                        }
+
+                    }
+                    throw;
+
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        #endregion
+    }
+}
--- ContosoUniversity/ContosoUniversity/Models/ContosoDbInitializer.cs
+++ ContosoUniversity/ContosoUniversity/Models/ContosoDbInitializer.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web;
 
 namespace ContosoUniversity.Models
 {
 
-    public class ContosoDbInitializer : DropCreateDatabaseAlways<ContosoUniversityContext>
+    public class ContosoDbInitializer : DropCreateDatabaseIfModelChanges<ContosoUniversityContext>
     {
 
         /// <summary>
@@ -17,96 +18,129 @@ namespace ContosoUniversity.Models
         protected override void Seed(ContosoUniversityContext dbCtx)
         {
 
-
-            List<Student> students = new List<Student>();
-            students.Add(new Student()
-            {
-                LastName = "Mtz",
-                FirstName = "Esau",
-                EnrollmentDate = DateTime.Now
-
-            });
-
-            students.Add(new Student()
+            try
             {
 
-                LastName = "Garcia",
-                FirstName = "Abby",
-                EnrollmentDate = DateTime.Now
+                #region List - Student
+                List<Student> students = new List<Student>();
+                students.Add(new Student()
+                {
+                    LastName = "Mtz",
+                    FirstName = "Esau",
+                    EnrollmentDate = DateTime.Now
 
-            });
+                });
 
-            students.Add(new Student()
+                students.Add(new Student()
+                {
+
+                    LastName = "Garcia",
+                    FirstName = "Abby",
+                    EnrollmentDate = DateTime.Now
+
+                });
+
+                students.Add(new Student()
+                {
+
+                    LastName = "Erasto",
+                    FirstName = "Sarahi",
+                    EnrollmentDate = DateTime.Now
+
+                });
+
+                dbCtx.Students.AddRange(students);
+
+                dbCtx.SaveChanges();
+                #endregion
+
+                #region List - Courses
+                List<Course> courses = new List<Course>();
+                courses.Add(new Course()
+                {
+
+                    Tittle = "Base de Datos",
+                    Credits = 5,
+
+                });
+
+                courses.Add(new Course()
+                {
+
+                    Tittle = "Programacion",
+                    Credits = 5,
+
+                });
+
+                courses.Add(new Course()
+                {
+
+                    Tittle = "Redes",
+                    Credits = 4,
+
+                });
+
+                dbCtx.Courses.AddRange(courses);
+
+                dbCtx.SaveChanges();
+                #endregion
+
+                #region List - Enrollments
+
+                List<Enrollment> enrollments = new List<Enrollment>();
+                enrollments.Add(new Enrollment()
+                {
+                    CourseId = 1,
+                    StudentId = 1,
+                    Grade = "Cuarto"
+
+                });
+
+                enrollments.Add(new Enrollment()
+                {
+                    CourseId = 2,
+                    StudentId = 2,
+                    Grade = "Cuarto"
+
+                });
+
+                enrollments.Add(new Enrollment()
+                {
+                    CourseId = 3,
+                    StudentId = 3,
+                    Grade = "Cuarto"
+
+                });
+
+                dbCtx.Enrollments.AddRange(enrollments);
+
+                dbCtx.SaveChanges();
+                #endregion
+
+            }
+            #region Catch
+            catch (DbEntityValidationException e)
             {
 
-                LastName = "Erasto",
-                FirstName = "Sarahi",
-                EnrollmentDate = DateTime.Now
+                foreach(var eve in e.EntityValidationErrors)
+                {
 
-            });
+                    Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors: ",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
 
-            dbCtx.Students.AddRange(students);
+                    foreach(var ve in eve.ValidationErrors)
+                    {
 
-            dbCtx.SaveChanges();
+                        Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                            ve.PropertyName, ve.ErrorMessage);
 
-            List<Course> courses = new List<Course>();
-            courses.Add(new Course()
-            {
+                    }
 
-                Tittle = "Base de Datos",
-                Credits = 5,
+                }
+                throw;
 
-            });
-
-            courses.Add(new Course()
-            {
-
-                Tittle = "Programacion",
-                Credits = 5,
-
-            });
-
-            courses.Add(new Course()
-            {
-
-                Tittle = "Redes",
-                Credits = 4,
-
-            });
-
-            dbCtx.Courses.AddRange(courses);
-
-            dbCtx.SaveChanges();
-
-            List<Enrollment> enrollments = new List<Enrollment>();
-            enrollments.Add(new Enrollment()
-            {
-                CourseId = 1,
-                StudentId = 1,
-                Grade = "Cuarto"
-
-            });
-
-            enrollments.Add(new Enrollment()
-            {
-                CourseId = 2,
-                StudentId = 2,
-                Grade = "Cuarto"
-
-            });
-
-            enrollments.Add(new Enrollment()
-            {
-                CourseId = 3,
-                StudentId = 3,
-                Grade = "Cuarto"
-
-            });
-
-            dbCtx.Enrollments.AddRange(enrollments);
-
-            dbCtx.SaveChanges();
-
+            }
+            #endregion
 
             base.Seed(dbCtx);
 
